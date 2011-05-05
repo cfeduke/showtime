027f751d937b66f810d1541ff279f85195653732
Charles Feduke <charles.feduke@gmail.com>
2011-05-05 12:11:43 -0400

    rails new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'cc6f837260ed9e0f50b088f73fcc078f9ffd14d84e9eaef7f214dc70d030c6c8fee4b78a0ff3a79d1775efc3562e559c7f2af75058ced3063e1f51799eec8f9c'
